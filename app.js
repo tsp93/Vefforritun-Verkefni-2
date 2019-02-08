f91ce696450976d4540a4d25b41deeaedc9be480
T-Beard <thomasspalsson@gmail.com>
2019-02-08 00:41:07 +0000

    Bunch of setup

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 
+const apply = require('./apply');
+const applications = require('./applications');
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -11,6 +14,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* todo aðrar stillingar á express appi */
+
+app.use('/', apply);
 
 function notFoundHandler(req, res, next) { // eslint-disable-line
   res.status(404).render('error', { title: '404', error: '404 fannst ekki' });
--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
 CREATE TABLE applications (
-  -- TODO schema fyrir töflu
+  id serial primary key,
+  name varchar(64) not null,
+  email varchar(64) not null,
+  phone int not null,
+  text text not null,
+  job varchar(14) not null,
+  processed boolean default false,
+  created timestamp with time zone not null default current_timestamp,
+  updated timestamp with time zone not null default current_timestamp
 );
