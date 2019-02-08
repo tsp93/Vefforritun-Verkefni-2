261ba86a3ace9310e12b89c75d75005d697e3fc0
T-Beard <thomasspalsson@gmail.com>
2019-02-08 17:53:44 +0000

    A lot of stuff

--- apply.js
+++ apply.js
@@ -1,9 +1,62 @@
+const xss = require('xss');
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+const { sanitize } = require('express-validator/filter');
+
+const { insert } = require('./db')
+
+const router = express.Router();
+
+const formValidation = [
+  check('name')
+    .isLength({ min: 1 })
+    .withMessage('Nafn má ekki vera tómt'),
+
+  check('email')
+    .isLength({ min: 1 })
+    .withMessage('Netfang má ekki vera tómt'),
+
+  check('email')
+    .isEmail()
+    .withMessage('Netfang verður að vera netfang'),
+
+  check('phone')
+    .matches(/^[0-9]{3}-?[0-9]{4}$/)
+    .withMessage('Símanúmer verður að vera sjö tölustafir'),
+
+  check('intro')
+    .isLength({ min: 100})
+    .withMessage('Kynning verður að vera að minnsta kosti 100 stafir'),
+
+  check('job')
+    .isIn(['programmer', 'designer', 'leader'])
+    .withMessage('Velja verður starf'),
+
+  sanitize('name')
+    .trim()
+    .escape(),
+  sanitize('email')
+    .normalizeEmail(),
+  sanitize('phone')
+    .blacklist('-')
+    .toInt(),
+  sanitize('intro')
+    .trim()
+    .escape(),
+];
 
 function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
-/* todo útfæra */
+async function list(req, res) {
+  const title = 'Umsókn';
+
+  res.render('apply', {
+    title,
+  });
+}
+
+router.get('/', catchErrors(list));
 
 module.exports = router;
--- db.js
+++ db.js
@@ -1,24 +1,81 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL; // sótt úr env gegnum dotenv pakka
-const client = new Client({
-  connectionString,
-});
+const connectionString = process.env.DATABASE_URL || 'postgres://:@localhost/v2';
 
-async function insert(name) {
-  client.connect();
+async function insert(data) {
+  const client = new Client({ connectionString });
+
+  await client.connect();
+
+  const query = 'INSERT INTO applications(name, email, ssn, amount) VALUES($1, $2, $3, $4)';
+  const values = [data.name, data.email, data.ssn, data.amount];
+
   try {
-    const query = 'INSERT INTO students (name) VALUES ($1)';
-    const res = await client.query(query, [name]);
-    console.info(res.rows);
+    await client.query(query, values);
   } catch (err) {
-    console.error(err);
-
-    // hér vantaði að kasta villu áfram sem var valdur að villu í lok sýnidæmis
+    console.error('Error inserting data');
     throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+async function fetch() {
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  try {
+    const result = await client.query('SELECT * FROM applications');
+
+    const { rows } = result;
+    return rows;
+  } catch (err) {
+    console.error('Error selecting form data');
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+async function update(data) {
+  const client = new Client({ connectionString });
+
+  await client.connect();
+
+  try {
+    const result = await client.query(query);
+
+    const { rows } = result;
+    return rows;
+  } catch (err) {
+    console.error('Error running query');
+    throw err;
+  } finally {
+    await client.end();
+  }
+}
+
+async function remove(data) {
+  const client = new Client({ connectionString });
+
+  await client.connect();
+
+  try {
+    const result = await client.query(query);
+
+    const { rows } = result;
+    return rows;
+  } catch (err) {
+    console.error('Error running query');
+    throw err;
+  } finally {
+    await client.end();
   }
 }
 
 module.exports = {
   insert,
+  fetch,
+  update,
+  remove,
 };
